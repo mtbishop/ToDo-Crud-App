e9703c13bfec3e90502efbcd39e63d6b37d62c3a
mtbishop <bishopmatthew76@gmail.com>
2021-12-17 14:18:52 -0700

    marking checkbox complete or not complete

--- script.js
+++ script.js
@@ -19,4 +19,30 @@ function addTodo() {
   render();
 }
 
-function render() {}
+function render() {
+  $('#todoList').innerHTML = null;
+
+  const todos = localStorage.getItem('todos');
+  todoList = JSON.parse(todos) || [];
+
+  for (let i = 0; i < todoList.length; i++) {
+    const item = document.createElement('li');
+    const checkbox = document.createElement('input');
+
+    checkbox.type = 'checkbox';
+    checkbox.on('click', function (e) {
+      todoList[i].completed = e.target.checked;
+      localStorage.setItem('todos', JSON.stringify(todoList));
+
+      if (todoList[i].completed) {
+        item.classList.add('completed');
+        item.classList.remove('uncompleted');
+        checkbox.checked = todoList[i].completed;
+      } else {
+        item.classList.add('uncompleted');
+        item.classList.remove('completed');
+        checkbox.checked = todoList[i].completed;
+      }
+    });
+  }
+}
