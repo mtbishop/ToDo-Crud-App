c730e4caf7dbc8f94bf9099b8a1e5752608b65f7
mtbishop <bishopmatthew76@gmail.com>
2021-12-30 15:15:07 -0700

    submit updates state and resets state to empty value

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList />
     </div>
   );
 }
